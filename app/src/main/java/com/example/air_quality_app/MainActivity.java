91017c402325057a25968324b46106e70b72660f
gothic459 <d.sek464@gmail.com>
2021-03-30 02:56:23 +0200

    top section - search bar and location need some tweaking, middle section added a bubble progress bar that changes colours depending on the progress number (right now a fixed value 0-100.0), bottom section - tbd

--- app/src/main/java/com/example/air_quality_app/MainActivity.java
+++ app/src/main/java/com/example/air_quality_app/MainActivity.java
@@ -5,22 +5,30 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.Manifest;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.SearchView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.xw.repo.BubbleSeekBar;
 
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
@@ -29,13 +37,73 @@ public class MainActivity extends AppCompatActivity {
     private final int REQUEST_LOCATION_PERMISSION = 1;
     private FusedLocationProviderClient fusedLocationClient;
     private double[] user_localization;
-    EditText editTextCity;
+    SearchView editTextCity;
+    TextView cityInput;
+    SeekBar seekBar;
+    BubbleSeekBar bseekBar;
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editTextCity = findViewById(R.id.editTextCity);
+        cityInput = findViewById(R.id.cityInput);
+        bseekBar = findViewById(R.id.bubbleSeek);
+        editTextCity.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if(query.length() != 0){
+                    Log.v("city:query:  ",query);
+                    cityInput.setText(query);
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+
+
+    bseekBar.setOnProgressChangedListener(new BubbleSeekBar.OnProgressChangedListener() {
+        @Override
+        public void onProgressChanged(BubbleSeekBar bubbleSeekBar, int progress, float progressFloat, boolean fromUser) {
+            if (!fromUser) {
+                String color = "#FFFFFF";
+
+                if (progress < 20) {
+                    color = "#9ef300";
+                } else if (progress < 40) {
+                    color = "#09af14";
+                } else if (progress < 60) {
+                    color = "#ffff00";
+                } else if (progress < 80) {
+                    color = "#ffb700";
+                } else if (progress <= 100) {
+                    color = "#ff0000";
+                }
+               // bseekBar.setBackgroundColor(Color.argb(255, r, g, b));
+                bseekBar.setBackgroundColor(Color.parseColor(color));
+                bseekBar.setTrackColor(Color.parseColor(color));
+                bseekBar.setSecondTrackColor(Color.parseColor(color));
+                bseekBar.setBubbleColor(Color.parseColor(color));
+            }
+        }
+
+        @Override
+        public void getProgressOnActionUp(BubbleSeekBar bubbleSeekBar, int progress, float progressFloat) {
+
+        }
+
+        @Override
+        public void getProgressOnFinally(BubbleSeekBar bubbleSeekBar, int progress, float progressFloat, boolean fromUser) {
+
+        }
+    });
 
     }
 
@@ -58,12 +126,23 @@ public class MainActivity extends AppCompatActivity {
             }
     }
 
-    public void intentActivityFromText(View view) {
-        String city = editTextCity.getText().toString();
+
+    public void intentActivityFromText(String query) {
         Intent intent = new Intent(this, AirQualityActivity.class);
-        intent.putExtra("key", city);
+        intent.putExtra("key", query);
         startActivity(intent);
     }
+    int num = 0;
+    public void testBtn(View view){
+        if (num<=100){
+            bseekBar.setProgress(num);
+        }else{
+            num =0;
+        }
+
+        num +=20;
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -85,7 +164,5 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     }
-
-
 
 }
